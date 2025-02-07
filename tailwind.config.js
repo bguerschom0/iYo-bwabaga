ad0033363f4bd772c29031cb906b4802f51494a0
bguerschom0 <bguerschom0@gmail.com>
2025-02-07 19:14:50 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        sandbeige: {
+          50: '#FDFCFB',
+          100: '#F5E6D3',
+          200: '#E8D5C4',
+          300: '#DACBB7',
+          400: '#C2B2A3',
+          800: '#4A3F35',
+          900: '#2C2522',
+        }
+      }
+    }
+  },
+  plugins: [],
+}
