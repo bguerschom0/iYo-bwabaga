17693cfd9981d92f382f4e4fafc4182af2740be5
bguerschom0 <bguerschom0@gmail.com>
2025-02-08 10:47:58 +0200

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  extends: [
+    'next/core-web-vitals',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended'
+  ],
+  plugins: ['react', 'react-hooks'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  }
+};
